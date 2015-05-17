e4a7a43f8c000e1ed2ee9ce0e1c7d1afc2f1a881
smturek <smturek@gmail.com>
2015-05-17 00:14:16 -0600

    inital commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var game = new Phaser.Game(940, 540, Phaser.AUTO, 'game',
+  { preload: preload, create: create, update: update });
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
